103e3b3deedd5dd8045bc356ccbf7afc3b7cb223
montylubana09 <montylubana51@gmail.com>
2023-04-05 10:35:31 -0600

    Submit form working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,39 @@
-// import { useState } from "react";
+import { useState } from "react";
 import React from "react";
 
 const Contact = () => {
-  // const [name, setName] = useState();
-  // const [email, setEmail] = useState();
-  // const [message, setMessage] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const clearForm = () => {
-    alert("Thank you for connecting. I will be in touch soon!");
-    // setName("");
-    // setEmail("");
-    // setMessage("");
-  };
+  const clearForm = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("message", message);
+  
+  fetch("https://getform.io/f/0e8a0aac-36bf-4d9e-b83f-cd70be71e459", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Form submitted successfully");
+        alert("Thank you for connecting. I will be in touch soon!");
+        setName("");
+        setEmail("");
+        setMessage("");
+      } else {
+        console.error("Form submission failed");
+        // do something after form submission fails, such as showing an error message
+      }
+    })
+    .catch((error) => {
+      console.error("Error submitting form:", error);
+    });
+};
+
 
   return (
     <div
@@ -31,34 +53,40 @@ const Contact = () => {
             target="_blank"
             action="https://getform.io/f/0e8a0aac-36bf-4d9e-b83f-cd70be71e459"
             method="POST"
-            //onSubmit={clearForm}
+            onSubmit={clearForm}
             className="flex flex-col w-full md:w-1/2"
           >
             <input
               type="text"
               name="name"
               placeholder="Enter you name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
               type="text"
               name="email"
+              value={email}
               placeholder="Enter you email"
+              onChange={(event) => setEmail(event.target.value)}
               className=" my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
 
             <textarea
               name="message"
               placeholder="Enter your message"
+              value={message}
               rows="10"
+              onChange={(event) => setMessage(event.target.value)}
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
             <button
+              type="submit"
               className="text-white bg-gradient-to-b from-cyan-500
              to-blue-500 px-6 py-3 my-8 mx-auto flex items-center
               rounded-md hover:scale-110 duration-300"
-              onClick={clearForm}
             >
               Let's Talk
             </button>
