026dd72bc270a87213a705a42ae570ca99295e3b
montylubana09 <montylubana51@gmail.com>
2023-04-11 11:42:53 -0600

    Adding the links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,38 +1,41 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+import LuminousLiquor from "../assets/portfolio/LuminousLiquor.png";
+import YYCWinter from "../assets/portfolio/YYCWinter.png";
+import FoodieApp from "../assets/portfolio/FoodieApp.png";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: LuminousLiquor,
+      demo: null,
+      code: null,
     },
     {
       id: 2,
-      src: installNode,
+      src: YYCWinter,
+      demo: "https://drive.google.com/file/d/1Ht-y_zmkRBzTiOyn-cq_Xr6-t6Z7NeRT/view?usp=share_link",
+      code: "https://github.com/jmeng-sys-hackathon/hackathon-frontend",
     },
     {
       id: 3,
-      src: navbar,
-    },
-    {
-      id: 4,
-      src: reactParallax,
-    },
-    {
-      id: 5,
-      src: reactSmooth,
-    },
-    {
-      id: 6,
-      src: reactWeather,
+      src: FoodieApp,
+      demo: "https://drive.google.com/file/d/1UyhlDQANmGJVk44AILqp06jgTebQocmf/view?usp=share_link",
+      code: "https://github.com/montylubana09/FoodieApp",
     },
   ];
+
+  const handleDemoClick = (link) => {
+    if (link) {
+      window.open(link, "_blank");
+    }
+  };
+
+  const handleCodeClick = (link) => {
+    if (link) {
+      window.open(link, "_blank");
+    }
+  };
   return (
     <div
       name="portfolio"
@@ -46,7 +49,7 @@ const Portfolio = () => {
           <p className="py-6">Check out some of my work right here</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, demo, code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -54,10 +57,22 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  disabled={!demo}
+                  onClick={() => handleDemoClick(demo)}
+                  title={!demo ? "Demo link unavailable" : ""}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  disabled={!code}
+                  onClick={() => handleCodeClick(code)}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                  title={
+                    !code ? alert("Link unavailable (Organization Code)") : ""
+                  }
+                >
                   Code
                 </button>
               </div>
