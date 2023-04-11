911e28642cf77f3df45f63419d4591fb66ecbcb5
montylubana09 <montylubana51@gmail.com>
2023-04-11 12:12:02 -0600

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,30 @@ const About = () => {
           </p>
         </div>
         <p className="text-xl mt-20 ">
-        As a software developer with over two years of experience in the field, I have focused primarily on developing applications using the React and React Native platforms. My expertise lies in building applications in the MERN stack, which includes MongoDB, ExpressJS, ReactJS, and NodeJS.
-
-During my career, I have developed a deep understanding of the React and React Native frameworks and have become proficient in using the latest development tools and technologies to create high-quality, efficient applications. I have experience working on a wide range of projects, including web applications, mobile applications, and complex enterprise solutions.
+          As a software developer with over two years of experience in the
+          field, I have focused primarily on developing applications using the
+          React and React Native platforms. My expertise lies in building
+          applications in the MERN stack, which includes MongoDB, ExpressJS,
+          ReactJS, and NodeJS.
+          <br />
+          During my career, I have developed a deep understanding of the React
+          and React Native frameworks and have become proficient in using the
+          latest development tools and technologies to create high-quality,
+          efficient applications. I have experience working on a wide range of
+          projects, including web applications, mobile applications, and complex
+          enterprise solutions.
         </p>
         <br />
 
         <p className="text-xl">
-        My experience as a software developer has equipped me with the skills, knowledge, and expertise necessary to take on challenging development projects and deliver high-quality results.
+          In addition to my technical skills, I have strong problem-solving
+          abilities and a keen eye for detail. I enjoy working collaboratively
+          with teams and am skilled at communicating complex technical concepts
+          to non-technical stakeholders.
+          <br />
+          My experience as a software developer has equipped me with the skills,
+          knowledge, and expertise necessary to take on challenging development
+          projects and deliver high-quality results.
         </p>
       </div>
     </div>
